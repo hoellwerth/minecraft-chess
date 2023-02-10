1d1ae94f83cfe259b3762451c1598ad3b53742a8
Johannes Höllwerth <johannes.hoellwerth@protonmail.com>
2023-02-10 17:41:18 +0100

    Initial Commit

---
+++ src/main/java/eu/baumistlustig/chess/Chess.java
@@ -0,0 +1,17 @@
+package eu.baumistlustig.chess;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Chess extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
