f16add54083f96d7e7e4b711d1e82c1752077c5e
Johannes Höllwerth <johannes.hoellwerth@protonmail.com>
2023-03-02 23:11:26 +0100

    Added ItemBuilder.createGuiItem

---
+++ src/main/java/eu/baumistlustig/chess/utils/ItemBuilder.java
@@ -0,0 +1,25 @@
+package eu.baumistlustig.chess.utils;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+
+public class ItemBuilder {protected ItemStack createGuiItem(final Material material, final String name, final String... lore) {
+    final ItemStack item = new ItemStack(material, 1);
+    final ItemMeta meta = item.getItemMeta();
+
+    // Set the name of the item
+    assert meta != null;
+    meta.setDisplayName(name);
+
+    // Set the lore of the item
+    meta.setLore(Arrays.asList(lore));
+
+    item.setItemMeta(meta);
+
+    return item;
+}
+
+}
