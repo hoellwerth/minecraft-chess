9eebd2e86be1d0f82a2729893822578c3e03810e
Johannes Höllwerth <johannes.hoellwerth@protonmail.com>
2023-03-02 23:21:53 +0100

    Added Timer

--- src/main/java/eu/baumistlustig/chess/Chess.java
+++ src/main/java/eu/baumistlustig/chess/Chess.java
@@ -1,17 +1,29 @@
 package eu.baumistlustig.chess;
 
+import eu.baumistlustig.chess.commands.Test;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class Chess extends JavaPlugin {
+    private static Chess plugin;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        plugin = this;
 
+         // Commands
+        Objects.requireNonNull(getCommand("chess")).setExecutor(new Test());
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static Plugin getInstance() {
+        return plugin;
     }
 }
