351b7a0b75b77aa07ca1de356995555346192c46
Johannes Höllwerth <johannes.hoellwerth@protonmail.com>
2023-03-29 21:32:49 +0200

    Added PluginManager

--- src/main/java/eu/baumistlustig/chess/Chess.java
+++ src/main/java/eu/baumistlustig/chess/Chess.java
@@ -1,7 +1,9 @@
 package eu.baumistlustig.chess;
 
 import eu.baumistlustig.chess.commands.Test;
+import eu.baumistlustig.chess.events.GuiListener;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
@@ -16,6 +18,11 @@ public final class Chess extends JavaPlugin {
 
          // Commands
         Objects.requireNonNull(getCommand("chess")).setExecutor(new Test());
+
+        // Events
+        PluginManager pluginManager = getServer().getPluginManager();
+
+        pluginManager.registerEvents(new GuiListener(), this);
     }
 
     @Override
